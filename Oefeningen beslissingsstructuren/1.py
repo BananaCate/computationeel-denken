64430915712eaaa4a5e7ce1a843c70cc5b54f55c
BananaCate <110566395+BananaCate@users.noreply.github.com>
2025-10-08 08:32:56 +0200

    oef 1 en 2 en verstoppen van een setup folder

--- Oefeningen beslissingsstructuren/1.py
+++ Oefeningen beslissingsstructuren/1.py
@@ -1,2 +1,11 @@
 leeftijd: int = int(input("Wat is jouw leeftijd? "))
-print(leeftijd)
+if leeftijd < 2:
+    print("Je bent een baby")
+elif leeftijd < 13:
+    print("Je bent een kind")
+elif leeftijd < 18:
+    print("Je bent een minderjarige tiener")
+elif leeftijd < 20:
+    print("Je bent een meerderjarige tiener")
+else:
+    print ("Je bent een volwassene")
